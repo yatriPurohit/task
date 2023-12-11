40d35955e4b6b768b2068de1e4e4c43dbd0d8eac
yatri Purohit <112961068+yatriPurohit@users.noreply.github.com>
2023-12-11 16:42:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// index.js
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');  // Import the 'path' module
 const UserController = require('./controllers/userController');
 const multer = require('multer');
 const app = express();
@@ -13,29 +13,16 @@ mongoose.connect('mongodb://localhost:27017/mydatabase', {
 
 app.use(express.json());
 
-// Multer storage configuration
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Serve HTML pages
+// Use express.static to serve static HTML files from the same directory
 app.use(express.static(__dirname));
 
-// Registration endpoint
 app.post('/register', upload.single('image'), UserController.register);
-
-// Login endpoint
 app.post('/login', UserController.login);
-
-// Profile page route
-app.get('/profile', UserController.getProfilePage);
-
-// Login page route
-app.get('/', UserController.getLoginPage);
-
-// API endpoint to fetch user data
 app.get('/api/user/:userId', UserController.fetchUserData);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: 'Internal Server Error' });
